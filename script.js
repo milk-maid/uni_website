45fc8c772bd0837e8af462e9b75e1b549307c02a
Bello Roqeeb <roqbell@gmail.com>
2022-09-06 16:52:54 +0100

    I'll get there

---
+++ script.js
@@ -0,0 +1,10 @@
+// for the menu icons functionality i.e TOGGLE
+var links = document.querySelector("#navLinks");
+
+function showMenu() {
+    links.style.right = "0";
+}
+
+function hideMenu() {
+    links.style.right = "-200px";
+}
